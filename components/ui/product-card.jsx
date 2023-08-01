8f12ce8baa6eb0c39220323a5d59133affaf7b47
Alvaro <alvarord519@gmail.com>
2023-08-01 14:44:19 -0500

    optimizations

--- components/ui/product-card.jsx
+++ components/ui/product-card.jsx
@@ -1,14 +1,18 @@
 "use client"
 
+import Image from "next/image";
+
 
 const ProductCard = ({ data }) => {
 
   return (
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       <div className="aspect-square rounded-xl bg-gray-100 relative">
-        <img
+        <Image
           src={data?.img}
           alt=""
+          height={290}
+          width={290}
           className="aspect-square object-cover rounded-md"
         />
       </div>
